22369a7b13a8f49104eb979cc35061bf11c851f3
Baranemlek <75400163+Baranemlek@users.noreply.github.com>
2020-12-03 04:57:22 +0300

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -20,26 +20,19 @@ from haruka.modules.helper_funcs.misc import paginate_modules
 from haruka.modules.translations.strings import tld, tld_help 
 from haruka.modules.connection import connected
 
-PM_START = """Hello {}, my name is {}!
+PM_START = """Merhaba {}, my name is {}!
 
-You know how hard it is sometimes to manage group so here is the solution for you
+Komutlar için /help
 
-I'm group manager bot and Anti-spam for RR Players🃏
+Bu bot grub yönetim botudur
 
-Activate Anti-spam protection in your BM by this command : 
-/antispam on 
+Yetki vermeniz gerekir aksi halde botu sürekli başlatmanız gerekir yetkisi yokken mesajlara iletişimi yoktur
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Botun sahibi yani ben @Baran_X_T
 
-Special thanks to [this person](t.me/denver02) for helping me.
+@mr_flymann a teşşekkürler
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
 
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
-
-Click /help or Help button below to find out more about how to use me to my full potential.
-
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
 """
 
 
